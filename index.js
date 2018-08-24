2b81b9e2e409a18241d75f36f3315a313eb6bf1e
Sandes de Silva <sdesilva@tribalscale.com>
2018-08-24 18:20:55 -0400

    Change export methods

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const helloWorld = (req, res) =>  {
+// const helloWorld = (req, res) =>  {
+//     const message = 'hello world';
+//     res.status(200).send(message);
+// }
+
+// module.exports = helloWorld;
+
+exports.helloWorld = function(req, res) {
     const message = 'hello world';
     res.status(200).send(message);
 }
-
-module.exports = helloWorld;
